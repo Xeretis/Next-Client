e60026e7d97ac0a119db2e10f1d8a75d2c8073c4
Xeretis <70800211+Xeretis@users.noreply.github.com>
2021-05-06 12:04:07 +0200

    Added logger + NextEvent

--- src/main/java/me/lor3mipsum/next/Main.java
+++ src/main/java/me/lor3mipsum/next/Main.java
@@ -1,11 +1,18 @@
 package me.lor3mipsum.next;
 
+import me.zero.alpine.bus.EventBus;
+import me.zero.alpine.bus.EventManager;
 import net.fabricmc.api.ModInitializer;
-import org.yaml.snakeyaml.Yaml;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class Main implements ModInitializer {
+
+	public static final Logger LOG = LogManager.getLogger("Next");
+	public static final EventBus EVENT_BUS = new EventManager();
+
 	@Override
 	public void onInitialize() {
-		System.out.println("Hello Fabric world!");
+		LOG.info("Initializing the client");
 	}
 }
---
+++ src/main/java/me/lor3mipsum/next/api/event/NextEvent.java
@@ -0,0 +1,17 @@
+package me.lor3mipsum.next.api.event;
+
+import me.zero.alpine.event.type.Cancellable;
+
+public class NextEvent extends Cancellable {
+    private Era era = Era.PRE;
+
+    public NextEvent(Era era) {
+        this.era = era;
+    }
+
+    public enum Era {
+        PRE,
+        PERI,
+        POST
+    }
+}
