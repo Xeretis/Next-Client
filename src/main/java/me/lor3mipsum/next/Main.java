1afa1be7431402dafa6114884d86e82b63f46ada
Xeretis <70800211+Xeretis@users.noreply.github.com>
2021-05-06 10:53:00 +0200

    Renamed default stuff

---
+++ src/main/java/me/lor3mipsum/next/Main.java
@@ -0,0 +1,10 @@
+package me.lor3mipsum.next;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
