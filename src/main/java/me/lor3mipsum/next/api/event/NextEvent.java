dce7e5299c5c16d2df4be320d7f2e96b22eb9cc1
Xeretis <70800211+Xeretis@users.noreply.github.com>
2021-05-20 16:22:15 +0200

    Added tickDelta to NextEvent

--- src/main/java/me/lor3mipsum/next/api/event/NextEvent.java
+++ src/main/java/me/lor3mipsum/next/api/event/NextEvent.java
@@ -1,9 +1,11 @@
 package me.lor3mipsum.next.api.event;
 
 import me.zero.alpine.event.type.Cancellable;
+import net.minecraft.client.MinecraftClient;
 
 public class NextEvent extends Cancellable {
     public Era era = Era.PRE;
+    public float tickDelta = MinecraftClient.getInstance().getTickDelta();
 
     public NextEvent() { }
 
