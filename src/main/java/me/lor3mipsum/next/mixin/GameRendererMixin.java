9515cdcd12442ee6479844c2818b2b406e7c5fde
Xeretis <70800211+Xeretis@users.noreply.github.com>
2021-05-23 11:33:17 +0200

    Added NoEntityTrace

--- src/main/java/me/lor3mipsum/next/mixin/GameRendererMixin.java
+++ src/main/java/me/lor3mipsum/next/mixin/GameRendererMixin.java
@@ -4,10 +4,14 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import me.lor3mipsum.next.Main;
 import me.lor3mipsum.next.api.event.game.RenderHurtcamEvent;
 import me.lor3mipsum.next.api.event.game.RenderShaderEvent;
+import me.lor3mipsum.next.client.impl.modules.exploit.NoEntityTrace;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.ShaderEffect;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.hit.HitResult;
 import org.lwjgl.opengl.GL11;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,6 +22,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Shadow private ShaderEffect shader;
+
+    @Shadow @Final private MinecraftClient client;
 
     @Inject(method = "bobViewWhenHurt", at = @At("HEAD"), cancellable = true)
     private void onBobViewWhenHurt(MatrixStack matrixStack, float f, CallbackInfo info) {
@@ -48,5 +54,13 @@ public class GameRendererMixin {
         }
 
         return null;
+    }
+
+    @Inject(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/ProjectileUtil;raycast(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;D)Lnet/minecraft/util/hit/EntityHitResult;"), cancellable = true)
+    private void onUpdateTargetedEntity(float tickDelta, CallbackInfo info) {
+        if (Main.moduleManager.getModule(NoEntityTrace.class).canWork() && client.crosshairTarget.getType() == HitResult.Type.BLOCK) {
+            client.getProfiler().pop();
+            info.cancel();
+        }
     }
 }
