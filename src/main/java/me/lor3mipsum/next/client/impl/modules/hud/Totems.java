8684aee2a440849cb6550742c62d62209250bc02
Xeretis <70800211+Xeretis@users.noreply.github.com>
2021-05-23 16:18:00 +0200

    Totems + Crapples + Gapples + Crystals

--- src/main/java/me/lor3mipsum/next/client/impl/modules/hud/Totems.java
+++ src/main/java/me/lor3mipsum/next/client/impl/modules/hud/Totems.java
@@ -4,30 +4,33 @@ import com.lukflug.panelstudio.hud.HUDList;
 import com.lukflug.panelstudio.hud.ListComponent;
 import com.lukflug.panelstudio.setting.Labeled;
 import com.lukflug.panelstudio.theme.ITheme;
-import me.lor3mipsum.next.Main;
 import me.lor3mipsum.next.api.util.misc.NextColor;
+import me.lor3mipsum.next.api.util.player.InventoryUtils;
 import me.lor3mipsum.next.client.core.gui.NextGui;
 import me.lor3mipsum.next.client.core.module.Category;
 import me.lor3mipsum.next.client.core.module.HudModule;
 import me.lor3mipsum.next.client.core.module.annotation.HudMod;
 import me.lor3mipsum.next.client.core.module.annotation.Mod;
 import me.lor3mipsum.next.client.impl.settings.ColorSetting;
+import net.minecraft.item.Items;
 import net.minecraft.util.Formatting;
 
 import java.awt.*;
 
-@Mod(name = "Watermark", description = "The client's watermark", category = Category.HUD)
-@HudMod(posX = 52, posY = 20)
-public class Watermark extends HudModule {
+@Mod(name = "Totems", description = "The number of totems in your inventory", category = Category.HUD)
+@HudMod(posX = 0, posY = 120)
+public class Totems extends HudModule {
 
     ColorSetting color = new ColorSetting("Color", false, new NextColor(255, 255, 255));
 
     @Override
     public void populate(ITheme theme) {
-        component = new ListComponent(new Labeled(getName(),null,()->true), position, getName(), new WatermarkList(), NextGui.FONT_HEIGHT, 1);
+        component = new ListComponent(new Labeled(getName(),null,()->true), position, getName(), new TotemList(), NextGui.FONT_HEIGHT, 1);
     }
 
-    private class WatermarkList implements HUDList {
+    private class TotemList implements HUDList {
+
+        public int totems = 0;
 
         @Override
         public int getSize() {
@@ -36,7 +39,8 @@ public class Watermark extends HudModule {
 
         @Override
         public String getItem(int index) {
-            return Main.CLIENT_NAME + " " + Formatting.GRAY + Main.CLIENT_VERSION;
+            totems = InventoryUtils.findItemInAll(Items.TOTEM_OF_UNDYING).count;
+            return "Totems: " + Formatting.GRAY + totems;
         }
 
         @Override
